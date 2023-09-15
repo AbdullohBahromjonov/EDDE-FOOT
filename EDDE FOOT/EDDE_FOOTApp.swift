b337a6d2d3d2b50e8d416056d2aaedb00bd4c6b5
Abdulloh <abdullohbahromjonv@gmail.com>
2023-09-15 15:42:58 +0500

    Initial Commit

---
+++ EDDE FOOT/EDDE_FOOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDDE_FOOTApp.swift
+//  EDDE FOOT
+//
+//  Created by Abdulloh on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EDDE_FOOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
